d3eadfc1d504aa181ba744510b716191da31536f
Tucq <tucq88@gmail.com>
2016-08-12 12:51:48 +0700

    Add GA code

--- wp-content/themes/dosomething/index.php
+++ wp-content/themes/dosomething/index.php
@@ -114,5 +114,17 @@ $assetUri = get_template_directory_uri() . '/assets';
         <script src="<?= $assetUri ?>/js/util.js"></script>
         <script src="<?= $assetUri ?>/js/main.js"></script>
 
+        <!-- Google Analytics -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-82389234-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+
     </body>
 </html>
